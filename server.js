7341c7f929bd80c048a660f07906793714911fda
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-07-15 00:09:53 -0400

    connect to mySQL database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: 'Pereira0802!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // default response for any other request (not found)
 app.use((req, res) => {
